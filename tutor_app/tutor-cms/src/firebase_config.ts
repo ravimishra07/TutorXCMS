83f36451b3ac8f9163c2a29c75bab9ef2ae33729
sam10172270 <ravimishra4services@gmail.com>
2025-05-07 17:41:57 +0530

    feat: restructure project with single git repository for both modules

---
+++ tutor_app/tutor-cms/src/firebase_config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    "projectId": "projectsam-9d34e",
+    "appId": "1:270412335979:web:2600bdb3b83f8044bb9a2e",
+    "databaseURL": "https://projectsam-9d34e-default-rtdb.firebaseio.com",
+    "storageBucket": "projectsam-9d34e.firebasestorage.app",
+    "apiKey": "AIzaSyD9hlDz-1LGrWAULK1ZkGsdxPEnRG-x9qg",
+    "authDomain": "projectsam-9d34e.firebaseapp.com",
+    "messagingSenderId": "270412335979",
+    "measurementId": "G-EYXTJLHNL9"
+}
